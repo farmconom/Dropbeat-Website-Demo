53ceee83a0461e48d8cc60b4a01670890d8aec35
farmconom <farmconom@gmail.com>
2023-03-18 00:28:08 +0700

    fix preload img

---
+++ js/preload-img.js
@@ -0,0 +1,13 @@
+const images = ['../images/dancer-bg.png',
+                '../images/icon-logo.jpg',
+                '../images/img-1.png',
+                '../images/img-2.png',
+                '../images/img-3.png',
+                '../images/img-4.png',
+                '../images/img-5.png',
+                '../images/logo.png'];
+           
+images.forEach((image) => {
+  const img = new Image();
+  img.src = image;
+});
