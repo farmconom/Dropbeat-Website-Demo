176ff8ff8594b97413f403e84fe52e947f9d1c34
farmconom <farmconom@gmail.com>
2023-03-18 01:47:12 +0700

    fix preload

--- js/preload-img.js
+++ js/preload-img.js
@@ -1,11 +1,11 @@
-const images = ['../images/dancer-bg.png',
-                '../images/icon-logo.jpg',
-                '../images/img-1.png',
-                '../images/img-2.png',
-                '../images/img-3.png',
-                '../images/img-4.png',
-                '../images/img-5.png',
-                '../images/logo.png'];
+const images = ['../../images/dancer-bg.png',
+                '../../images/icon-logo.jpg',
+                '../../images/img-1.png',
+                '../../images/img-2.png',
+                '../../images/img-3.png',
+                '../../images/img-4.png',
+                '../../images/img-5.png',
+                '../../images/logo.png'];
            
 images.forEach((image) => {
   const img = new Image();
